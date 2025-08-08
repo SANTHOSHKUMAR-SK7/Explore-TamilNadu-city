61f049514b607827300684183f5770267c8caa5c
Santhoshkumar <santhoshsk7077@gmail.com>
2025-08-08 14:44:35 +0530

    Class main file

--- Smart_City_TN/src/Main.java
+++ Smart_City_TN/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class Main {
     public static void main(String[] args) {
@@ -43,3 +43,4 @@ public class Main {
         }
     }
 }
+
